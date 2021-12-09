e9298d154f7ce2e4e57e1623dbb624f04ece89d6
Michal Pokorný <93346591+MichalPKN@users.noreply.github.com>
2021-12-09 20:23:37 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const para = document.getElementById("para");
-//const parall = document.getElementById("parall");
-
-window.addEventListener("scroll", function() {
-    let offset = window.pageYOffset;
-    //let value = window.scrollY
-    para.style.backgroundPositionY = offset * 0.6 + "px";
-    //parall.style.top = value * 666 + "px";
-})
