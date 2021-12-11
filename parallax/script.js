b32c70c4b5dd18ef5b19cb738c63d7e3216ac0c7
Michal Pokorný <93346591+MichalPKN@users.noreply.github.com>
2021-12-11 16:58:39 +0100

    Add files via upload

--- parallax/script.js
+++ parallax/script.js
@@ -1,9 +1,22 @@
 const para = document.getElementById("para");
-//const parall = document.getElementById("parall");
+const chleb1 = document.getElementById("chleb1");
+const chleb2 = document.getElementById("chleb2");
+const chleb22 = document.getElementById("chleb22");
+const chleb3 = document.getElementById("chleb3");
 
-window.addEventListener("scroll", function() {
-    let offset = window.pageYOffset;
-    //let value = window.scrollY
-    para.style.backgroundPositionY = offset * 0.6 + "px";
-    //parall.style.top = value * 666 + "px";
-})
+document.onscroll = () => {
+    console.log(chleb2.style.right);
+    let value = window.scrollY;
+    chleb1.style.filter = "hue-rotate(" + value * 0.7 + "deg)";
+    para.style.backgroundPositionY = value * 0.6 + "px";
+    chleb2.style.transform = "rotate(" + value * 1 + "deg)";
+    chleb2.style.left = value * 0.5 + "px";
+    chleb2.style.bottom = value * 0.9 + "px";
+    chleb22.style.transform = "rotate(" + -value * 1 + "deg)";
+    chleb22.style.right = value * 0.5 + "px";
+    chleb22.style.bottom = value * 0.9 + "px";
+    chleb3.style.bottom = value * 0.2 + "px";
+    size = 15 + value * 0.02;
+    chleb3.style.marginLeft = -(size / 2) + "em";
+    chleb3.style.maxWidth = size + "em";
+}
